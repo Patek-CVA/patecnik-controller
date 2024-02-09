3338ce4f3a70b17944143b1e48d4bc0eea6a155e
merole <erik.sab18@gmail.com>
2024-02-09 15:08:09 +0100

    sockets

--- src/routes/app/app.tsx
+++ src/routes/app/app.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 export default function App(): React.JSX.Element {
+    const [balls, setBalls] = React.useState(0);
+
+    const WS_URL = "ws://192.168.4.1:4443";
+    const socket = new WebSocket(WS_URL);
+
+    socket.addEventListener('open', (event) => {
+        console.log('Connected to server');
+    });
+
+    socket.addEventListener('message', (event) => {
+        console.log('Message from server ', event.data);
+        setBalls(parseInt(event.data));
+    });
+
+
+
+
     return (
         <>
-            <h1>Hello World!</h1>
+            <h1>Balls</h1>
+            <h2 id='balls'>{ balls }</h2>
+
         </>
     );
 }
