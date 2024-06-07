ba3aeeeaf908fe74e7744877d4105803172fb78b
Patai5 <lukyn.patai5@gmail.com>
2024-06-07 15:23:29 +0200

    fix: add life feed web sockets useEffect hook

--- src/components/rover-controller/live-feed/live-feed.tsx
+++ src/components/rover-controller/live-feed/live-feed.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppContext } from '../../app-context';
 import './live-feed.scss';
 
@@ -6,9 +6,11 @@ export default function LiveFeed(): React.JSX.Element {
     const { webSocketManager } = React.useContext(AppContext);
     const [liveImage, setLiveImage] = React.useState<string | null>(null);
 
-    webSocketManager.subscribe(['live-feed'], (message) => {
-        setLiveImage(message.data.image);
-    });
+    useEffect(() => {
+        webSocketManager.subscribe(['live-feed'], (message) => {
+            setLiveImage(message.data.image);
+        });
+    }, [webSocketManager]);
 
     const liveFeedInner = liveImage ? (
         <img src={liveImage} alt={'Live Feed'} className="live-feed-image" />
